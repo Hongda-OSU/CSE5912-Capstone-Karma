cf1ee1e5396f4dd10c80a1f656ce51a37026fbcb
JasonLian123 <lian.109@buckeyemail.osu.edu>
2022-01-27 00:20:23 -0500

    Add basic movement AI for enemy.

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -6,18 +6,36 @@ using UnityEngine.AI;
 public class EnemyController : MonoBehaviour
 {
     private float lookRadius = 10f;
+    private float distance;
 
     Transform target;
     NavMeshAgent agent;
 
     void Start()
     {
-        
+        target = PlayerManager.instance.player.transform;
+        agent = GetComponent<NavMeshAgent>();
     }
 
     void Update()
     {
-        
+        distance = Vector3.Distance(target.position, transform.position);
+
+        if (distance <= lookRadius) {
+            agent.SetDestination(target.position);
+
+            if (distance <= agent.stoppingDistance) {
+                // Attack player.
+                FaceTarget();
+            }
+        }
+    }
+
+    private void FaceTarget() { 
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
     }
 
     private void OnDrawGizmosSelected()
