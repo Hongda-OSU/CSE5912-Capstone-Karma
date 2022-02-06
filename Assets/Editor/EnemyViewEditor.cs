1d312543190e16869eb0589a2cdae9349e2fc57a
Baihua <yang.4562@osu.edu>
2022-02-06 18:46:36 -0500

    Merge

---
+++ Assets/Editor/EnemyViewEditor.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Skeleton_1))]
+
+public class EnemyViewEditor : Editor
+{
+    public GameObject closestTarget;
+    // float closestDistance = 9999;
+
+    void OnSceneGUI()
+    {
+        Skeleton_1 fow = (Skeleton_1)target;
+        Handles.color = Color.white;
+        Vector3 viewAngleA = fow.DirFromAngle(-fow.GetViewAngle() / 2, false);
+        Vector3 viewAngleB = fow.DirFromAngle(fow.GetViewAngle() / 2, false);
+
+        // Test code.
+        // Handles.DrawWireArc(Vector3.zero, Vector3.up, Vector3.forward, 360, 10);
+
+        Handles.DrawWireArc(fow.GetTransform().position, Vector3.up, fow.DirFromAngle(-fow.GetViewAngle(), false) + fow.GetTransform().forward, fow.GetViewAngle(), fow.GetViewRadius());
+        Handles.DrawWireArc(fow.GetTransform().position, Vector3.up, Vector3.forward, 360, fow.GetCloseDetectionDistance());
+
+        Handles.DrawLine(fow.GetTransform().position, fow.GetTransform().position + viewAngleA * fow.GetViewRadius());
+        Handles.DrawLine(fow.GetTransform().position, fow.GetTransform().position + viewAngleB * fow.GetViewRadius());
+
+        Handles.color = Color.red;
+        if (fow.FoundTarget()) {
+            Handles.DrawLine(fow.GetTransform().position, fow.GetTargetPosition());
+        }
+    }
+}
