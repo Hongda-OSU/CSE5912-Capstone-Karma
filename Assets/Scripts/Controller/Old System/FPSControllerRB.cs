b79376bcd8e6b64561d39334d7668a0b826d5603
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2022-02-18 04:18:54 -0500

    save work

--- Assets/Scripts/Controller/Old System/FPSControllerRB.cs
+++ Assets/Scripts/Controller/Old System/FPSControllerRB.cs
@@ -21,23 +21,25 @@ public class FPSControllerRB : MonoBehaviour
     public float Gravity;
 
     private Transform characterTransform;
-    private Vector3 movementDirection;
+    private Vector3 currentMovementDirection;
+    private Vector3 currentVelocity;
 
     private float currentSpeed;
     private float originHeight;
+    private float characterSpeed;
+    private float horizontalInput, verticalInput;
 
-    private bool isGrounded = true;
+    private bool isGrounded;
     private bool isCrouched;
 
-    private WaitForSeconds waitOneSeconds = new WaitForSeconds(0.1f);
+    private WaitForSeconds waitForSeconds = new WaitForSeconds(0.1f);
     [SerializeField] private ParticleSystem forwardDashParticle;
     [SerializeField] private ParticleSystem backwardDashParticle;
 
-    private float horizontalInput, verticalInput;
-
-
+    
     private void Start()
     {
+        isGrounded = true;
         characterTransform = transform;
         characterRigidbody = GetComponent<Rigidbody>();
         capsuleCollider = GetComponent<CapsuleCollider>();
@@ -50,51 +52,85 @@ public class FPSControllerRB : MonoBehaviour
     {
         if (isGrounded)
         {
-            var tmp_IsCrouchKeyDown = Input.GetKeyDown(KeyCode.C);
-            var tmp_IsSprinting = Input.GetKey(KeyCode.LeftShift);
+            var isJumpKeyDown = Input.GetButtonDown("Jump");
+            var isCrouchKeyDown = Input.GetKeyDown(KeyCode.C);
+            var isDashKeyDown = Input.GetKeyDown(KeyCode.V);
+            var isSprinting = Input.GetKey(KeyCode.LeftShift);
 
-            currentSpeed = tmp_IsSprinting ? SprintingSpeed : WalkSpeed;
-
-            if (Input.GetKeyDown(KeyCode.C))
+            // Handle Jump
+            if (isJumpKeyDown)
+            {
+                characterRigidbody.velocity = new Vector3(currentVelocity.x, CalculateJumpHeightSpeed(),
+                    currentVelocity.z);
+               
+                if (characterAnimator)
+                    characterAnimator.SetTrigger("Jump");
+            }
+            
+            // Handle Crouch
+            if (isCrouchKeyDown)
             {
                 var tmp_CurrentHeight = isCrouched ? originHeight : CrouchHeight;
                 StartCoroutine(DoCrouch(tmp_CurrentHeight));
                 isCrouched = !isCrouched;
             }
 
-            if (isCrouched)
-                currentSpeed = Input.GetKey(KeyCode.LeftShift) ? SprintingSpeedWhenCrouched : WalkSpeedWhenCrouched;
-            else
-                currentSpeed = Input.GetKey(KeyCode.LeftShift) ? SprintingSpeed : WalkSpeed;
-
-            if (Input.GetButtonDown("Jump"))
+            // Handle Dash only when running
+            if (isDashKeyDown && characterRigidbody.velocity.magnitude > 5.0f && Math.Abs(horizontalInput) != 1)
             {
-                characterRigidbody.velocity = new Vector3(movementDirection.x, CalculateJumpHeightSpeed(),
-                    movementDirection.z);
+                currentSpeed = DashSpeed;
+                PlayDashParticle();
             }
-        }
-    }
+            else
+            {
+                if (isCrouched)
+                    currentSpeed = isSprinting ? SprintingSpeedWhenCrouched : WalkSpeedWhenCrouched;
+                else
+                    currentSpeed = isSprinting ? SprintingSpeed : WalkSpeed;
+            }
 
+            HandleAnimation();
+        }
+        //else
+        //{
+        //    characterRigidbody.AddForce(new Vector3(0, -Gravity * characterRigidbody.mass, 0));
+        //}
+    }
 
     private void FixedUpdate()
     {
-        var tmp_Horizontal = Input.GetAxis("Horizontal");
-        var tmp_Vertical = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
 
-        var tmp_CurrentDirection = new Vector3(tmp_Horizontal, 0, tmp_Vertical);
-        tmp_CurrentDirection = characterTransform.TransformDirection(tmp_CurrentDirection);
-        tmp_CurrentDirection *= currentSpeed;
+        currentMovementDirection = new Vector3(horizontalInput, 0, verticalInput);
+        currentMovementDirection = characterTransform.TransformDirection(currentMovementDirection);
+        currentMovementDirection *= currentSpeed;
 
         var tmp_CurrentVelocity = characterRigidbody.velocity;
-        movementDirection = tmp_CurrentVelocity;
-        var tmp_VelocityChange = tmp_CurrentDirection - tmp_CurrentVelocity;
+        currentVelocity = tmp_CurrentVelocity;
+        var tmp_VelocityChange = currentMovementDirection - tmp_CurrentVelocity;
         tmp_VelocityChange.y = 0;
-
         characterRigidbody.AddForce(tmp_VelocityChange, ForceMode.VelocityChange);
+    }
+
+    private void HandleAnimation()
+    {
+        var tmp_Velocity = characterRigidbody.velocity;
+        tmp_Velocity.y = 0;
+        characterSpeed = tmp_Velocity.magnitude;
+        if (Math.Abs(horizontalInput) > 0 && Math.Abs(verticalInput) > 0)
+            characterSpeed /= (float)Math.Sqrt(2);
+        if (characterAnimator != null)
+        {
+            if (Input.GetKeyDown(KeyCode.L))
+                characterAnimator.SetTrigger("Inspect");
+            characterAnimator.SetFloat("Velocity", characterSpeed, 0.25f, Time.deltaTime);
+        }
     }
 
     private IEnumerator DoCrouch(float _targetHeight)
     {
+        yield return waitForSeconds;
         while (Math.Abs(capsuleCollider.height - _targetHeight) > 0.05f)
         {
             capsuleCollider.height =
@@ -106,6 +142,26 @@ public class FPSControllerRB : MonoBehaviour
     private float CalculateJumpHeightSpeed()
     {
         return Mathf.Sqrt(2 * Gravity * JumpHeight);
+    }
+
+    private void PlayDashParticle()
+    {
+        if (verticalInput > 0 && Math.Abs(horizontalInput) <= verticalInput)
+        {
+            forwardDashParticle.Play();
+            return;
+        }
+        if (verticalInput < 0 && Math.Abs(horizontalInput) <= Math.Abs(verticalInput))
+        {
+            backwardDashParticle.Play();
+            return;
+        }
+        forwardDashParticle.Play();
+    }
+
+    internal void SetupAnimator(Animator aniamtor)
+    {
+        characterAnimator = aniamtor;
     }
 
     private void OnCollisionStay(Collision _other)
