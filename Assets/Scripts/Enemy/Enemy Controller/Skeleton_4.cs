3a922b1afc7e99a6a2163e6e8d014e26faeafc53
JasonLian123 <lian.109@buckeyemail.osu.edu>
2022-02-23 01:17:10 -0500

    Add enemies to the level

---
+++ Assets/Scripts/Enemy/Enemy Controller/Skeleton_4.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CSE5912.PolyGamers
+{
+    public class Skeleton_4 : RegularEnemy
+    {
+        void Update()
+        {
+        
+        }
+
+        protected override void HandleDeath()
+        {
+
+        }
+
+        protected override void HandlePatrol()
+        {
+
+        }
+
+        protected override void HandleWander()
+        {
+
+        }
+    }
+}
