13138f5a7a5d130a01dc1993792c79ab47c24402
Baihua <ybhbaihua668@163.com>
2022-03-08 21:15:01 -0500

    new skill - perish

--- Assets/Scripts/Skill/Skills_elements/Passive/Perish.cs
+++ Assets/Scripts/Skill/Skills_elements/Passive/Perish.cs
@@ -7,5 +7,35 @@ namespace CSE5912.PolyGamers
     public class Perish : Skill
     {
 
+        [Header("Perish")]
+        [SerializeField] private GameObject vfxPrefab;
+
+        [SerializeField] private float baseTriggerHealth = 0.05f;
+        [SerializeField] private float triggerHealthPerLevel = 0.015f;
+
+
+        private void Update()
+        {
+            var target = PlayerManager.Instance.HitByBullet;
+            if (!isLearned || target == null || !target.IsAlive)
+                return;
+
+            bool isTriggered = target.Health / target.MaxHealth <= baseTriggerHealth + triggerHealthPerLevel * (level - 1);
+            if (target.Infected.Stack < target.Infected.MaxStack || !isTriggered)
+                return;
+
+            Perform(target);
+        }
+
+        private void Perform(Enemy target)
+        {
+            GameObject vfx = Instantiate(vfxPrefab);
+            vfx.transform.position = target.transform.position + Vector3.up * target.GetComponentInChildren<Renderer>().bounds.size.y / 2;
+            Destroy(vfx, 5f);
+
+            float dmg = target.Health * 10f;
+            Damage damage = new Damage(dmg, Element.Type.Venom, PlayerStats.Instance, target);
+            PlayerManager.Instance.PerformSkillDamage(target, damage);
+        }
     }
 }
