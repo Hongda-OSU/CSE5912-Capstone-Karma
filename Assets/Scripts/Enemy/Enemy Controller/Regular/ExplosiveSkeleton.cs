5989b703d676e4a9df44e6a005e7a1a3e7b06439
JasonLian123 <lian.109@buckeyemail.osu.edu>
2022-03-16 15:10:36 -0400

    Start working on explosive skeleton

---
+++ Assets/Scripts/Enemy/Enemy Controller/Regular/ExplosiveSkeleton.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CSE5912.PolyGamers
+{
+    public class ExplosiveSkeleton : RegularEnemy
+    {
+        protected override void PerformActions()
+        {
+
+        }
+
+        protected override void PlayDeathAnimation()
+        {
+
+        }
+
+        protected override void HandlePatrol()
+        {
+
+        }
+
+        protected override void HandleWander()
+        {
+
+        }
+    }
+}
