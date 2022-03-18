6dc0207c074eba461f2ceb6591885b73bd421f7e
Baihua <ybhbaihua668@163.com>
2022-03-18 12:17:13 -0400

    hud bug fixed

--- Assets/Scripts/UI/HUD/CrosshairContorl.cs
+++ Assets/Scripts/UI/HUD/CrosshairContorl.cs
@@ -46,21 +46,24 @@ namespace CSE5912.PolyGamers
 
             crosshair.style.display = DisplayStyle.Flex;
 
-            // if player is moving without aiming or shooting, then crosshair become more bigger
-            if (isMoving && !weaponManager.isAiming && !weaponManager.isFiring)
-                currentSize = Mathf.Lerp(currentSize, MaxSize, Time.deltaTime * speed);
+            if (weaponManager.CarriedWeapon.CurrentAmmo > 0)
+            {
+                // if player is moving without aiming or shooting, then crosshair become more bigger
+                if (isMoving && !weaponManager.isAiming && !weaponManager.isFiring)
+                    currentSize = Mathf.Lerp(currentSize, MaxSize, Time.deltaTime * speed);
 
-            // if player is moving without aiming but shooting, then crosshair become bigger
-            else if (isMoving && !weaponManager.isAiming && !weaponManager.isFiring)
-                currentSize = Mathf.Lerp(currentSize, (MaxSize + FiringSize) / 2, Time.deltaTime * speed);
+                // if player is moving without aiming but shooting, then crosshair become bigger
+                else if (isMoving && !weaponManager.isAiming && !weaponManager.isFiring)
+                    currentSize = Mathf.Lerp(currentSize, (MaxSize + FiringSize) / 2, Time.deltaTime * speed);
 
-            // if player is shooting without aiming, then crosshair become smaller
-            else if (weaponManager.isFiring && !weaponManager.isAiming)
-                currentSize = Mathf.Lerp(currentSize, FiringSize, Time.deltaTime * speed);
+                // if player is shooting without aiming, then crosshair become smaller
+                else if (weaponManager.isFiring && !weaponManager.isAiming)
+                    currentSize = Mathf.Lerp(currentSize, FiringSize, Time.deltaTime * speed);
 
-            // if player is aiming, then crosshair become more smaller
-            else if (weaponManager.isAiming)
-                crosshair.style.display = DisplayStyle.None;
+                // if player is aiming, then crosshair become more smaller
+                else if (weaponManager.isAiming)
+                    crosshair.style.display = DisplayStyle.None;
+            }
 
             // else, change to original size
             else
