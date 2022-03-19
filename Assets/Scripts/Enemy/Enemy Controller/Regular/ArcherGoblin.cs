b2fb2d2980312bd83981bc60940ed4f8c5ea9f20
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2022-03-18 20:51:25 -0400

    Archer Goblin Finished

--- Assets/Scripts/Enemy/Enemy Controller/Regular/ArcherGoblin.cs
+++ Assets/Scripts/Enemy/Enemy Controller/Regular/ArcherGoblin.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using UnityEngine;
 
 namespace CSE5912.PolyGamers
@@ -14,7 +13,6 @@ namespace CSE5912.PolyGamers
         {
             if (playerDetected)
                 FaceTarget(directionToPlayer);
-
             switch (status)
             {
                 case Status.Idle:
@@ -30,35 +28,49 @@ namespace CSE5912.PolyGamers
                     break;
 
                 case Status.Moving:
-                    //if (distanceToPlayer < closeDetectionRange)
-                    //{
-                    //    Retreat();
-                    //}
+                    if (distanceToPlayer > 15f)
+                        MoveToPlayer();
 
                     if (!isAttacking)
                     {
                         isPlayingAttackAnim = true;
-                        
+                        // 3~8
                         if (isPlayerInAttackRange && distanceToPlayer > closeDetectionRange)
                         {
                             Attack(1);
                         }
+                        // 8~15
                         else if (distanceToPlayer <= 15f && distanceToPlayer > attackRange)
                         {
                             Attack(2);
                         }
-                        else if (distanceToPlayer > 15f)
+                        else
                         {
                             isPlayingAttackAnim = false;
-                            MoveToPlayer();
                         }
                     }
                     break;
 
                 case Status.Attacking:
-                    if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && !animator.IsInTransition(0))
+                    if (distanceToPlayer <= 5f)
                     {
-                        Debug.Log("Finished");
+                        animator.SetTrigger("RollBackward");
+                        if (counter == 0)
+                        {
+                            animator.SetTrigger("DodgeLeft");
+                        }
+                        else if (counter > 0)
+                        {
+                            animator.SetTrigger("DodgeRight");
+                        }
+                    }
+                    else if (distanceToPlayer <= 12f && distanceToPlayer > attackRange)
+                    {
+                        if (counter == 0)
+                        {
+                            if(Random.value > 0.3f)
+                                animator.SetTrigger("RollForward");
+                        }
                     }
                     status = Status.Moving;
                     break;
@@ -83,33 +95,43 @@ namespace CSE5912.PolyGamers
 
         private void Shoot()
         {
+            counter++;
             if (counter == 3)
                 TrippleShooting();
             else
             {
-                GameObject vfx = Instantiate(arrowVFX, transform.position + transform.forward + new Vector3(0, 0.8f, 0), Quaternion.identity);
+                GameObject vfx = Instantiate(arrowVFX, transform.position + transform.forward * 0.8f + new Vector3(0, 1f, 0), Quaternion.identity);
                 // arrow follow player direction
                 vfx.transform.LookAt(PlayerManager.Instance.Player.transform.position);
-                Destroy(vfx, 5f);
+                Destroy(vfx, 4f);
             }
-            counter++;
+        }
+
+        private void Reload()
+        {
+            arrow.SetActive(true);
+        }
+
+        private void ReloadFinish()
+        {
+            arrow.SetActive(false);
         }
 
         private void TrippleShooting()
         {
-            GameObject vfx1 = Instantiate(arrowVFX, transform.position + transform.forward + new Vector3(0, 0.8f, 0), Quaternion.identity);
-            GameObject vfx2 = Instantiate(arrowVFX, transform.position + transform.forward + new Vector3(0, 0.8f, 0), Quaternion.identity);
-            GameObject vfx3 = Instantiate(arrowVFX, transform.position + transform.forward + new Vector3(0, 0.8f, 0), Quaternion.identity);
+            GameObject vfx1 = Instantiate(arrowVFX, transform.position + transform.forward * 0.8f + new Vector3(0, 1f, 0), Quaternion.identity);
+            GameObject vfx2 = Instantiate(arrowVFX, transform.position + transform.forward * 0.8f + new Vector3(0, 1f, 0), Quaternion.identity);
+            GameObject vfx3 = Instantiate(arrowVFX, transform.position + transform.forward * 0.8f + new Vector3(0, 1f, 0), Quaternion.identity);
 
-            Vector3 angle_1 = DirFromAngle(30f, false) * 10f;
-            Vector3 angle_2 = DirFromAngle(330f, false) * 10f;
+            Vector3 angle_1 = DirFromAngle(15f, false) * 10f;
+            Vector3 angle_2 = DirFromAngle(315f, false) * 10f;
 
             vfx1.transform.LookAt(PlayerManager.Instance.Player.transform.position + angle_1);
             vfx2.transform.LookAt(PlayerManager.Instance.Player.transform.position);
             vfx3.transform.LookAt(PlayerManager.Instance.Player.transform.position + angle_2);
-            Destroy(vfx1, 5f);
-            Destroy(vfx2, 5f);
-            Destroy(vfx3, 5f);
+            Destroy(vfx1, 4f);
+            Destroy(vfx2, 4f);
+            Destroy(vfx3, 4f);
             counter = 0;
         }
 
