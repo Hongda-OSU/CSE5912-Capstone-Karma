87bd205e316338d56bd5ab118847bf3db401b9ac
JasonLian123 <lian.109@buckeyemail.osu.edu>
2022-04-07 03:07:49 -0400

    Implement HellBlade Phase 1, working on phase transition

--- Assets/Scripts/Enemy/Enemy Controller/Boss/Golem.cs
+++ Assets/Scripts/Enemy/Enemy Controller/Boss/Golem.cs
@@ -61,7 +61,7 @@ namespace CSE5912.PolyGamers
 
             if (slowdownPlayer)
             {
-                if (!playerSlowed) 
+                if (!playerSlowed)
                 {
                     PlayerStats.Instance.MoveSpeedFactor -= 0.8f;
                     playerSlowed = true;
@@ -69,7 +69,7 @@ namespace CSE5912.PolyGamers
 
                 slowdownCounter -= Time.deltaTime;
 
-                if (slowdownCounter <= 0f) 
+                if (slowdownCounter <= 0f)
                 {
                     PlayerStats.Instance.MoveSpeedFactor = originSpeedFactor;
                     slowdownCounter = 3.5f;
@@ -78,7 +78,7 @@ namespace CSE5912.PolyGamers
                 }
             }
 
-            switch (status) 
+            switch (status)
             {
                 case Status.Idle:
 
@@ -94,7 +94,7 @@ namespace CSE5912.PolyGamers
 
                     break;
 
-                case Status.Moving:                 
+                case Status.Moving:
                     if (!isAttacking)
                     {
                         isPlayingAttackAnim = true;
@@ -102,7 +102,7 @@ namespace CSE5912.PolyGamers
                         if (Vector3.Distance(player.position, pivot_1.position) <= 30f && Vector3.Distance(player.position, pivot_1.position) >= 22f)
                         {
                             FaceTarget(directionToPlayer);
-                            Attack(1);                          
+                            Attack(1);
                         }
                         else if (isPlayerInAttackRange && distanceToPlayer > closeDetectionRange)
                         {
@@ -128,16 +128,17 @@ namespace CSE5912.PolyGamers
 
                     break;
 
-                case Status.Attacking:                   
+                case Status.Attacking:
                     if (isPlayingAttackAnim)
                     {
-                        
+
                         if (animator.GetCurrentAnimatorStateInfo(0).IsName("Sword_Attack_1") ||
                             animator.GetCurrentAnimatorStateInfo(0).IsName("Sword_Attack_2") ||
-                            animator.GetCurrentAnimatorStateInfo(0).IsName("Attack_1")) {
+                            animator.GetCurrentAnimatorStateInfo(0).IsName("Attack_1"))
+                        {
                             FaceTarget(directionToPlayer);
                         }
-                        
+
 
                         if (!(animator.GetCurrentAnimatorStateInfo(0).IsName("Attack_Stomp_1") ||
                             animator.GetCurrentAnimatorStateInfo(0).IsName("Attack_Stomp_2") ||
@@ -153,7 +154,7 @@ namespace CSE5912.PolyGamers
                             animator.ResetTrigger("Attack_4");
                         }
                     }
-                    else 
+                    else
                     {
                         if (isFatigued)
                         {
@@ -207,7 +208,8 @@ namespace CSE5912.PolyGamers
             yield return new WaitForSeconds(Time.deltaTime);
         }
 
-        public void ShardstoneShooting() {
+        public void ShardstoneShooting()
+        {
             GameObject vfx_1 = Instantiate(prefab_1, pivot_1.position, Quaternion.identity);
             GameObject vfx_2 = Instantiate(prefab_1, pivot_1.position, Quaternion.identity);
             GameObject vfx_3 = Instantiate(prefab_1, pivot_1.position, Quaternion.identity);
@@ -219,14 +221,14 @@ namespace CSE5912.PolyGamers
             vfx_2.transform.LookAt(new Vector3(PlayerManager.Instance.Player.transform.position.x, PlayerManager.Instance.Player.transform.position.y - 1.5f, PlayerManager.Instance.Player.transform.position.z));
             vfx_3.transform.LookAt(new Vector3(PlayerManager.Instance.Player.transform.position.x, PlayerManager.Instance.Player.transform.position.y - 1.5f, PlayerManager.Instance.Player.transform.position.z) + angle_2);
 
-            if (Vector3.Distance(player.position, pivot_1.position) <= 30f && 
+            if (Vector3.Distance(player.position, pivot_1.position) <= 30f &&
                 Vector3.Angle(transform.forward, directionToPlayer) < 60 / 2 &&
-                (PlayerManager.Instance.Player.transform.position.y <= vfx_2.transform.position.y + 2f)) 
+                (PlayerManager.Instance.Player.transform.position.y <= vfx_2.transform.position.y + 2f))
             {
                 Damage damage = new Damage(Random.Range(10f, 20f), Element.Type.Physical, this, PlayerStats.Instance);
                 PlayerStats.Instance.TakeDamage(damage);
 
-                if (PlayerStats.Instance.Health > 0f) 
+                if (PlayerStats.Instance.Health > 0f)
                 {
                     FPSControllerCC.Instance.AddImpact(this.gameObject.transform.TransformDirection(Vector3.forward), 100f);
                     FPSControllerCC.Instance.AddImpact(Vector3.up, 100f);
@@ -238,7 +240,8 @@ namespace CSE5912.PolyGamers
             Destroy(vfx_3, 4f);
         }
 
-        public void Shockwave() {
+        public void Shockwave()
+        {
             GameObject vfx = Instantiate(prefab_2);
             vfx.transform.position = new Vector3(transform.position.x, -0.5f, transform.position.z);
             //vfx.transform.LookAt(new Vector3(PlayerManager.Instance.Player.transform.position.x, 0f, PlayerManager.Instance.Player.transform.position.z));
@@ -246,22 +249,24 @@ namespace CSE5912.PolyGamers
             Damage damage = new Damage(Random.Range(3f, 5f), Element.Type.Physical, this, PlayerStats.Instance);
             PlayerStats.Instance.TakeDamage(damage);
 
-            if (PlayerStats.Instance.Health > 0f) { 
+            if (PlayerStats.Instance.Health > 0f)
+            {
                 FPSControllerCC.Instance.AddImpact(this.gameObject.transform.TransformDirection(Vector3.forward), 200f);
             }
 
             Destroy(vfx, 2f);
         }
 
-        public void Vine() {
+        public void Vine()
+        {
             GameObject vfx = Instantiate(prefab_3);
             GameObject dust = Instantiate(prefab_4);
             vfx.transform.position = new Vector3(PlayerManager.Instance.Player.transform.position.x, PlayerManager.Instance.Player.transform.position.y - 2f, PlayerManager.Instance.Player.transform.position.z);
             dust.transform.position = pivot_1.position;
 
-            if (PlayerManager.Instance.Player.transform.position.y < vfx.transform.position.y + 3f) 
+            if (PlayerManager.Instance.Player.transform.position.y < vfx.transform.position.y + 3f)
             {
-                Damage damage = new Damage(Random.Range(1f,5f), Element.Type.Venom, this, PlayerStats.Instance);
+                Damage damage = new Damage(Random.Range(1f, 5f), Element.Type.Venom, this, PlayerStats.Instance);
                 PlayerStats.Instance.TakeDamage(damage);
                 slowdownPlayer = true;
             }
@@ -270,7 +275,8 @@ namespace CSE5912.PolyGamers
             Destroy(dust, 2f);
         }
 
-        protected override void Hit() {
+        protected override void Hit()
+        {
             float damageAmount;
             if (distanceToPlayer <= attackRange)
             {
@@ -284,37 +290,12 @@ namespace CSE5912.PolyGamers
             }
         }
 
-        public void Landing() {
+        public void Landing()
+        {
             GameObject vfx = Instantiate(prefab_5);
             vfx.transform.position = new Vector3(transform.position.x, 0f, transform.position.z);
 
             Destroy(vfx, 4f);
-        }
-
-                // These codes below are used by Eiditor for testing purpose.
-        public Transform GetTransform()
-        {
-            return transform;
-        }
-
-        public float GetViewAngle()
-        {
-            return viewAngle;
-        }
-
-        public float GetViewRadius()
-        {
-            return viewRadius;
-        }
-
-        public float GetCloseDetectionDistance()
-        {
-            return closeDetectionRange;
-        }
-
-        public bool FoundTarget()
-        {
-            return foundTarget;
         }
 
         public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
@@ -325,13 +306,5 @@ namespace CSE5912.PolyGamers
             }
             return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
         }
-
-        /*
-        void OnDrawGizmosSelected()
-        {
-            Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(transform.position, viewRadius);
-        }
-        */
     }
 }
